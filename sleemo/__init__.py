ade8cea880bec0f11d46d78bae7a6bf34b3e38f3
twkiiim <twkiiim@gmail.com>
2020-10-27 01:45:55 +0900

    [release] v0.0.2

--- sleemo/__init__.py
+++ sleemo/__init__.py
@@ -2,7 +2,7 @@
 """AWS AppSync Lambda Application Framework"""
 
 __author__ = "Taewoo Kim <twkiiim@gmail.com>"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __status__ = "alpha"
-__date__ = "25 Oct 2020"
+__date__ = "26 Oct 2020"
 
