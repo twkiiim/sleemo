46c416577033567b5b15fdd2018775f8692c4c70
twkiiim <twkiiim@gmail.com>
2020-10-29 01:43:48 +0900

    sleemo release version updated

--- sleemo/__init__.py
+++ sleemo/__init__.py
@@ -2,7 +2,7 @@
 """AWS AppSync Direct Lambda Resolver Framework"""
 
 __author__ = "Taewoo Kim <twkiiim@gmail.com>"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __status__ = "alpha"
 __date__ = "29 Oct 2020"
 
